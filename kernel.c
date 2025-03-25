2635745afcdeee55b4653a7275c0626560d6a399
manchangfengxu <manchangfengxu@gmail.com>
2025-03-25 23:52:08 +0800

    Disk I/O

--- kernel.c
+++ kernel.c
@@ -43,6 +43,24 @@ struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
   return (struct sbiret){.error = a0, .value = a1};
 }
 
+// 访问 MMIO 寄存器,使用volatile,在 MMIO
+// 中，内存访问可能会触发副作用(例如，向设备发送命令)。
+uint32_t virtio_reg_read32(unsigned offset) {
+  return *((volatile uint32_t *)(VIRTIO_BLK_PADDR + offset));
+}
+
+uint64_t virtio_reg_read64(unsigned offset) {
+  return *((volatile uint64_t *)(VIRTIO_BLK_PADDR + offset));
+}
+
+void virtio_reg_write32(unsigned offset, uint32_t value) {
+  *((volatile uint32_t *)(VIRTIO_BLK_PADDR + offset)) = value;
+}
+
+void virtio_reg_fetch_and_or32(unsigned offset, uint32_t value) {
+  virtio_reg_write32(offset, virtio_reg_read32(offset) | value);
+}
+
 void putchar(char ch) {
   sbi_call(ch, 0, 0, 0, 0, 0, 0, 1 /* Console Putchar */);
 }
@@ -287,6 +305,9 @@ struct process *create_process(const void *image, size_t image_size) {
              PAGE_U | PAGE_R | PAGE_W | PAGE_X);
   }
 
+  // 将 virtio-blk MMIO 区域映射到页表中
+  map_page(page_table, VIRTIO_BLK_PADDR, VIRTIO_BLK_PADDR, PAGE_R | PAGE_W);
+
   proc->pid = i + 1;
   proc->state = PROC_RUNNABLE;
   proc->sp = (uint32_t)sp;
@@ -367,9 +388,132 @@ long getchar(void) {
   return ret.error;
 }
 
+struct virtio_virtq *blk_request_vq;
+struct virtio_blk_req *blk_req;
+paddr_t blk_req_paddr;
+unsigned blk_capacity;
+
+struct virtio_virtq *virtq_init(unsigned index) {
+  // 为 virtqueue 分配一个区域。
+  paddr_t virtq_paddr =
+      alloc_pages(align_up(sizeof(struct virtio_virtq), PAGE_SIZE) / PAGE_SIZE);
+  struct virtio_virtq *vq = (struct virtio_virtq *)virtq_paddr;
+  vq->queue_index = index;
+  vq->used_index = (volatile uint16_t *)&vq->used.index;
+  // 1. 通过写入队列的索引(第一个队列为 0)到 QueueSel 来选择队列。
+  virtio_reg_write32(VIRTIO_REG_QUEUE_SEL, index);
+  // 5. 通过写入大小到 QueueNum 来通知设备队列大小。
+  virtio_reg_write32(VIRTIO_REG_QUEUE_NUM, VIRTQ_ENTRY_NUM);
+  // 6. 通过写入其字节值到 QueueAlign 来通知设备已使用的对齐方式。
+  virtio_reg_write32(VIRTIO_REG_QUEUE_ALIGN, 0);
+  // 7. 将队列第一页的物理编号写入 QueuePFN 寄存器。
+  virtio_reg_write32(VIRTIO_REG_QUEUE_PFN, virtq_paddr);
+  return vq;
+}
+
+void virtio_blk_init(void) {
+  if (virtio_reg_read32(VIRTIO_REG_MAGIC) != 0x74726976)
+    PANIC("virtio: invalid magic value");
+  if (virtio_reg_read32(VIRTIO_REG_VERSION) != 1)
+    PANIC("virtio: invalid version");
+  if (virtio_reg_read32(VIRTIO_REG_DEVICE_ID) != VIRTIO_DEVICE_BLK)
+    PANIC("virtio: invalid device id");
+
+  // 1. 重置设备。
+  virtio_reg_write32(VIRTIO_REG_DEVICE_STATUS, 0);
+  // 2. 设置 ACKNOWLEDGE 状态位：表示客户操作系统已注意到该设备。
+  virtio_reg_fetch_and_or32(VIRTIO_REG_DEVICE_STATUS, VIRTIO_STATUS_ACK);
+  // 3. 设置 DRIVER 状态位。
+  virtio_reg_fetch_and_or32(VIRTIO_REG_DEVICE_STATUS, VIRTIO_STATUS_DRIVER);
+  // 5. 设置 FEATURES_OK 状态位。
+  virtio_reg_fetch_and_or32(VIRTIO_REG_DEVICE_STATUS, VIRTIO_STATUS_FEAT_OK);
+  // 7. 执行设备特定的设置，包括发现设备的 virtqueues
+  blk_request_vq = virtq_init(0);
+  // 8. 设置 DRIVER_OK 状态位。
+  virtio_reg_write32(VIRTIO_REG_DEVICE_STATUS, VIRTIO_STATUS_DRIVER_OK);
+
+  // 获取磁盘容量。
+  blk_capacity = virtio_reg_read64(VIRTIO_REG_DEVICE_CONFIG + 0) * SECTOR_SIZE;
+  printf("virtio-blk: capacity is %d bytes\n", blk_capacity);
+
+  // 分配一个区域来存储对设备的请求。
+  blk_req_paddr =
+      alloc_pages(align_up(sizeof(*blk_req), PAGE_SIZE) / PAGE_SIZE);
+  blk_req = (struct virtio_blk_req *)blk_req_paddr;
+}
+
+void virtq_kick(struct virtio_virtq *vq, int desc_index) {
+  vq->avail.ring[vq->avail.index % VIRTQ_ENTRY_NUM] = desc_index;
+  vq->avail.index++;
+  // 内存屏障,确保前面的内存写入操作在执行后续的操作之前被完全提交
+  __sync_synchronize();
+  virtio_reg_write32(VIRTIO_REG_QUEUE_NOTIFY, vq->queue_index);
+  vq->last_used_index++;
+}
+
+bool virtq_is_busy(struct virtio_virtq *vq) {
+  return vq->last_used_index != *(vq->used_index);
+}
+
+// 从 virtio-blk 设备读取/写入。
+void read_write_disk(void *buf, unsigned sector, int is_write) {
+  if (sector >= blk_capacity / SECTOR_SIZE) {
+    printf("virtio: tried to read/write sector=%d, but capacity is %d\n",
+           sector, blk_capacity / SECTOR_SIZE);
+    return;
+  }
+
+  // 根据 virtio-blk 规范构造请求。
+  blk_req->sector = sector;
+  blk_req->type = is_write ? VIRTIO_BLK_T_OUT : VIRTIO_BLK_T_IN;
+  if (is_write) memcpy(blk_req->data, buf, SECTOR_SIZE);
+
+  // 构造 virtqueue 描述符(使用 3 个描述符)。
+  struct virtio_virtq *vq = blk_request_vq;
+  // 第一个描述符：设备只读
+  vq->descs[0].addr = blk_req_paddr;
+  vq->descs[0].len = sizeof(uint32_t) * 2 + sizeof(uint64_t);
+  vq->descs[0].flags = VIRTQ_DESC_F_NEXT;
+  vq->descs[0].next = 1;
+  // 第二个描述符：如果是读操作则设备可写(VIRTQ_DESC_F_WRITE)
+  vq->descs[1].addr = blk_req_paddr + offsetof(struct virtio_blk_req, data);
+  vq->descs[1].len = SECTOR_SIZE;
+  vq->descs[1].flags = VIRTQ_DESC_F_NEXT | (is_write ? 0 : VIRTQ_DESC_F_WRITE);
+  vq->descs[1].next = 2;
+  // 第三个描述符：设备可写(VIRTQ_DESC_F_WRITE)
+  vq->descs[2].addr = blk_req_paddr + offsetof(struct virtio_blk_req, status);
+  vq->descs[2].len = sizeof(uint8_t);
+  vq->descs[2].flags = VIRTQ_DESC_F_WRITE;
+
+  // 通知设备有新的请求。
+  virtq_kick(vq, 0);
+
+  // 等待设备完成处理。
+  while (virtq_is_busy(vq)) {
+  }
+
+  // virtio-blk：如果返回非零值，则表示错误。
+  if (blk_req->status != 0) {
+    printf("virtio: warn: failed to read/write sector=%d status=%d\n", sector,
+           blk_req->status);
+    return;
+  }
+
+  // 对于读操作，将数据复制到缓冲区。
+  if (!is_write) memcpy(buf, blk_req->data, SECTOR_SIZE);
+}
+
 void kernel_main(void) {
   memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
   WRITE_CSR(stvec, (uint32_t)kernel_entry);
+
+  virtio_blk_init();
+  char buf[SECTOR_SIZE];
+  read_write_disk(buf, 0, false /* 从磁盘读取 */);
+  printf("first sector: %s\n", buf);
+
+  strcpy(buf, "hello from kernel!!!\n");
+  read_write_disk(buf, 0, true /* 写入磁盘 */);
 
   idle_proc = create_process(NULL, 0);
   idle_proc->pid = 0;
--- kernel.h
+++ kernel.h
@@ -39,6 +39,32 @@
 #define SCAUSE_ECALL 8
 #define PROC_EXITED 2
 
+#define SECTOR_SIZE 512
+#define VIRTQ_ENTRY_NUM 16
+#define VIRTIO_DEVICE_BLK 2
+#define VIRTIO_BLK_PADDR 0x10001000
+#define VIRTIO_REG_MAGIC 0x00
+#define VIRTIO_REG_VERSION 0x04
+#define VIRTIO_REG_DEVICE_ID 0x08
+#define VIRTIO_REG_QUEUE_SEL 0x30
+#define VIRTIO_REG_QUEUE_NUM_MAX 0x34
+#define VIRTIO_REG_QUEUE_NUM 0x38
+#define VIRTIO_REG_QUEUE_ALIGN 0x3c
+#define VIRTIO_REG_QUEUE_PFN 0x40
+#define VIRTIO_REG_QUEUE_READY 0x44
+#define VIRTIO_REG_QUEUE_NOTIFY 0x50
+#define VIRTIO_REG_DEVICE_STATUS 0x70
+#define VIRTIO_REG_DEVICE_CONFIG 0x100
+#define VIRTIO_STATUS_ACK 1
+#define VIRTIO_STATUS_DRIVER 2
+#define VIRTIO_STATUS_DRIVER_OK 4
+#define VIRTIO_STATUS_FEAT_OK 8
+#define VIRTQ_DESC_F_NEXT 1
+#define VIRTQ_DESC_F_WRITE 2
+#define VIRTQ_AVAIL_F_NO_INTERRUPT 1
+#define VIRTIO_BLK_T_IN 0
+#define VIRTIO_BLK_T_OUT 1
+
 typedef unsigned char uint8_t;
 typedef unsigned int uint32_t;
 typedef uint32_t size_t;
@@ -91,3 +117,53 @@ struct process {
   uint32_t *page_table;  // 页表
   uint8_t stack[8192];   // 内核栈
 };
+
+// Virtqueue Descriptor 区域条目。
+struct virtq_desc {
+  uint64_t addr;
+  uint32_t len;
+  uint16_t flags;
+  uint16_t next;
+} __attribute__((packed));
+
+// Virtqueue Available Ring。
+struct virtq_avail {
+  uint16_t flags;
+  uint16_t index;
+  uint16_t ring[VIRTQ_ENTRY_NUM];
+} __attribute__((packed));
+
+// Virtqueue Used Ring 条目。
+struct virtq_used_elem {
+  uint32_t id;
+  uint32_t len;
+} __attribute__((packed));
+
+// Virtqueue Used Ring。
+struct virtq_used {
+  uint16_t flags;
+  uint16_t index;
+  struct virtq_used_elem ring[VIRTQ_ENTRY_NUM];
+} __attribute__((packed));
+
+// Virtqueue。
+struct virtio_virtq {
+  struct virtq_desc descs[VIRTQ_ENTRY_NUM];
+  struct virtq_avail avail;
+  struct virtq_used used __attribute__((aligned(PAGE_SIZE)));
+  int queue_index;  // 队列索引
+  volatile uint16_t *used_index;
+  uint16_t last_used_index;
+} __attribute__((packed));
+
+// Virtio-blk 请求。
+struct virtio_blk_req {
+  // 第一个描述符：设备只读
+  uint32_t type;
+  uint32_t reserved;
+  uint64_t sector;
+  // 第二个描述符：如果是读操作则设备可写(VIRTQ_DESC_F_WRITE)
+  uint8_t data[512];
+  // 第三个描述符：设备可写(VIRTQ_DESC_F_WRITE)
+  uint8_t status;
+} __attribute__((packed));
